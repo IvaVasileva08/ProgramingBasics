22662bd009ea5d59737bb7ccbd159e64b5588cf3
IvaVasileva08 <vasileva_iva_i@abv.bg>
2025-05-11 11:12:27 +0300

    Add files via upload

---
+++ ConcatenateData/Program.cs
@@ -0,0 +1,5 @@
+﻿string firstName = Console.ReadLine();
+string finalName = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+string town = Console.ReadLine();
+Console.WriteLine($"You are {firstName} {finalName}, a {age}-years old person from {town}.");
